d9b23da5f77d295e47b3bfacf3e6865d2bdc3009
Pandenimik <Pandenimik@mail.ru>
2021-12-28 14:58:04 +0300

    Creating a project/

---
+++ riddles/main.js
@@ -0,0 +1,26 @@
+var riddle = {
+    question: 'Висит груша нельзя скушать',
+    correctAnswer: 'лампочка',
+    hints: ['это съедобное', 'это фрукт'],
+    tries: 3,
+    checkAnswer() {
+        // TODO: написать логику проверки правильного ответа
+        // alert для пользователя, console.log()
+        
+    },
+}
+
+window.onload = function() {
+    document.getElementById('riddle').innerText = riddle.question;
+}
+
+function check() {
+    var input = document.getElementsByTagName('input')[0];
+
+    var guessedAnswer = input.value;
+
+    if (guessedAnswer) {
+        // TODO: вызвать функцию checkAnswer у объекта загадки, передав туда ответ
+        
+    }
+}
