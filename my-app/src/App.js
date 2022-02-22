830a855a01c20d9d8985165a42a0bbc2c12f584e
Pandenimik <pandenimik@mail.ru>
2022-02-22 07:48:24 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function App() {
 
-  function showMess(mess) {
-    alert(mess);
-  }
+  const arr = ['a', 'b', 'c', 'd', 'e'];
+
+  const result = arr.map(function(item, index) {
+    return <li key={index}>{item}</li>;
+  });
 
   return <div>
-    <button onClick={() => showMess(1)}>act1</button>
-    <button onClick={() => showMess(2)}>act2</button>
-    <button onClick={() => showMess(3)}>act3</button>
+    <ul>{result}</ul>
   </div>;
 }
 
