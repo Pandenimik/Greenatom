4d27f0f2f4a8afbe25071cae29b47742bd40debf
Pandenimik <pandenimik@mail.ru>
2022-02-22 14:14:59 +0300

    hgf

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
 
-  const arr = ['a', 'b', 'c', 'd', 'e'];
-
-  const result = arr.map(function(item, index) {
-    return <li key={index}>{item}</li>;
-  });
-
-  return <div>
-    <ul>{result}</ul>
-  </div>;
+  return Header();
 }
 
 export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return <>
+    <header className='main-header'>
+      Заголовок
+    </header>
+  </>
+} 
+
+export default Header;
